1e00e4c1379f2aeb9877bac01752704185cd08be
Caio <caiomand04@gmail.com>
2025-10-09 11:18:16 -0300

    Crei o Controle do Banco de Dados
    A classe ControleBancoDeDados faz a conexão com o banco de dados local e é possível executar Querys do SQL

--- LocAuto/src/ucb/projeto/controller/ControleBancoDeDados.java
+++ LocAuto/src/ucb/projeto/controller/ControleBancoDeDados.java
@@ -1,4 +1,31 @@
 package ucb.projeto.controller;
 
+import java.sql.*;
+
 public class ControleBancoDeDados {
+    private final String url = "jdbc:mysql://localhost:3306/locauto";
+    private final String usuario = "root";
+
+    private final Connection conexao;
+
+    public ControleBancoDeDados(String senha) throws SQLException {
+        conexao = DriverManager.getConnection(url, usuario, senha);
+    }
+
+    public Connection getConexao() {
+        return conexao;
+    }
+
+    // Retorna o resultado da query do SQL
+    public ResultSet executarCusultaBD(String sql) throws SQLException {
+        PreparedStatement statement = conexao.prepareStatement(sql);
+        return statement.executeQuery();
+    }
+
+    // Retorna o número de linhas afetadas
+    public int executarAtualizacaoBD(String sql) throws SQLException {
+        PreparedStatement statement = conexao.prepareStatement(sql);
+        return statement.executeUpdate();
+    }
+
 }
--- LocAuto/src/ucb/projeto/view/Main.java
+++ LocAuto/src/ucb/projeto/view/Main.java
@@ -1,9 +1,42 @@
 package ucb.projeto.view;
 
+import ucb.projeto.controller.ControleBancoDeDados;
 import ucb.projeto.model.Pessoa;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a senha do bando de dados: ");
+        String senha = scanner.nextLine();
 
+        try {
+            ControleBancoDeDados controler = new ControleBancoDeDados(senha);
+            System.out.println("Conexão bem sucedida");
+
+            String inserirSQL = "INSERT INTO tb_pessoa(cpf, nome, data_nasc, email) VALUES ('00011122212', 'Caio Rangel', '2010-10-5', 'test123@gmail.com')";
+            controler.executarAtualizacaoBD(inserirSQL);
+
+            ResultSet resultado = controler.executarCusultaBD("SELECT * FROM tb_pessoa");
+
+            while (resultado.next()) {
+                int id = resultado.getInt("id_pessoa");
+                String cpf = resultado.getString("cpf");
+                String nome = resultado.getString("nome");
+                String email = resultado.getString("email");
+
+                System.out.println("ID: " + id);
+                System.out.println("CPF " + cpf);
+                System.out.println("Nome: " + nome);
+                System.out.println("Email: " + email);
+            }
+
+        } catch (SQLException error) {
+            System.out.println("Falha ao se conectar com banco de dados " + error);
+            throw new RuntimeException(error);
+        }
     }
 }
